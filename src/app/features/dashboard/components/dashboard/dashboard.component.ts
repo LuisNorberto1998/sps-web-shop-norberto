f786d380f3f83f7e7e793635fc574bf6fa365f18
Norberto <luis_norberto1998@hotmail.com>
2024-08-13 14:05:10 -0600

    Modificación para usar componentes creados (lista-productos, header, footer, search-bar)

--- src/app/features/dashboard/components/dashboard/dashboard.component.ts
+++ src/app/features/dashboard/components/dashboard/dashboard.component.ts
@@ -1,33 +1,23 @@
-import { Component, OnInit } from '@angular/core';
-import { AuthService } from '../../../auth/services/auth.service';
-import { Router } from '@angular/router';
+import { Component, OnInit} from '@angular/core';
+
 import { HeaderComponent } from '../../../shared/components/header/header.component';
 import { FooterComponent } from '../../../shared/components/footer/footer.component';
 import { ProductListComponent } from '../../../products/components/product-list/product-list.component';
 
+import { SearchBarComponent } from '../../../shared/components/search-bar/search-bar.component';
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [HeaderComponent, FooterComponent, ProductListComponent],
+  imports: [
+    HeaderComponent,
+    FooterComponent,
+    ProductListComponent,
+    SearchBarComponent,
+  ],
   templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.scss'
+  styleUrl: './dashboard.component.scss',
 })
 export class DashboardComponent implements OnInit {
-
-  constructor(private authService: AuthService, private router: Router) {}
-
-  ngOnInit(): void {
-      
-  }
-
-  logout() {
-    this.authService.logout()
-      .then(response => {
-        console.log(response);
-        this.router.navigate(['/login'])
-      })
-      .catch(error => console.error(error)
-    );
-  }
-
+  constructor() {}
+  ngOnInit(): void {}
 }
