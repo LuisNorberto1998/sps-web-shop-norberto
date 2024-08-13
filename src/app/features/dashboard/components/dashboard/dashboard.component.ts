3e431a0a2258afc7365f98811a45de54713e1cb4
Norberto <luis_norberto1998@hotmail.com>
2024-08-12 20:49:02 -0600

    Creación e integración de header y footer

--- src/app/features/dashboard/components/dashboard/dashboard.component.ts
+++ src/app/features/dashboard/components/dashboard/dashboard.component.ts
@@ -1,11 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../../auth/services/auth.service';
 import { Router } from '@angular/router';
+import { HeaderComponent } from '../../../shared/components/header/header.component';
+import { FooterComponent } from '../../../shared/components/footer/footer.component';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent, FooterComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
