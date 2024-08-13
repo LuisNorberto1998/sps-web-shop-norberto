7842f952f671f4ef3321e4c2b837b9af2660878a
Norberto <luis_norberto1998@hotmail.com>
2024-08-13 14:02:39 -0600

    Se modifica el estilo del heder con integración de logout

--- src/app/features/shared/components/header/header.component.ts
+++ src/app/features/shared/components/header/header.component.ts
@@ -1,12 +1,33 @@
 import { Component } from '@angular/core';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { Router } from '@angular/router';
+import { AuthService } from '../../../auth/services/auth.service';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [MatToolbarModule, MatButtonModule, MatIconModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+
+  constructor(private authService: AuthService, private router: Router) {}
+
+  goToCart() {
+    this.router.navigate(['/cart'])
+  }
+
+  logout() {
+    this.authService.logout()
+      .then(response => {
+        console.log(response);
+        this.router.navigate(['/login'])
+      })
+      .catch(error => console.error(error)
+    );
+  }
 
 }
