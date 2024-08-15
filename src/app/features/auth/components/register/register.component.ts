2227d4a97733eef62ce404c7df82453cb04c4c6e
Norberto <luis_norberto1998@hotmail.com>
2024-08-14 18:25:09 -0600

    Diseño responsivo para moviles

--- src/app/features/auth/components/register/register.component.ts
+++ src/app/features/auth/components/register/register.component.ts
@@ -3,27 +3,47 @@ import {
   FormBuilder,
   FormControl,
   FormGroup,
+  FormsModule,
   ReactiveFormsModule,
   Validators,
 } from '@angular/forms';
 import { AuthService } from '../../services/auth.service';
 import { User } from '../../../../core/models/login.model';
 import { Router } from '@angular/router';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatIconModule } from '@angular/material/icon';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [
+    ReactiveFormsModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatDividerModule,
+    MatIconModule,
+    CommonModule,
+  ],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss',
 })
 export class RegisterComponent implements OnInit {
-  loginForm!: FormGroup;
+  registerForm!: FormGroup;
 
-  constructor(private fb: FormBuilder, private authService: AuthService, private router: Router) {}
+  constructor(
+    private fb: FormBuilder,
+    private authService: AuthService,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
-    this.loginForm = this.fb.group({
+    this.registerForm = this.fb.group({
       email: ['', [Validators.required, Validators.email]],
       password: ['', [Validators.required]],
     });
@@ -31,14 +51,14 @@ export class RegisterComponent implements OnInit {
 
   onSubmit() {
     const user: User = {
-      email: this.loginForm.value.email,
-      password: this.loginForm.value.password,
+      email: this.registerForm.value.email,
+      password: this.registerForm.value.password,
     };
 
     this.authService
       .register(user)
-      .then(response => {
-        this.router.navigate(['/login'])
+      .then((response) => {
+        this.router.navigate(['/login']);
       })
       .catch((error) => console.error(error));
   }
